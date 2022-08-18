1eef8f01d10c39761e4d27dca5368794123383b2
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-18 10:59:27 +0100

    complete create blog schemas

--- src/components/shop/component/Header.jsx
+++ src/components/shop/component/Header.jsx
@@ -2,24 +2,39 @@ import React from "react";
 import { useState } from "react";
 import { BiCartAlt } from "react-icons/bi";
 import { close, logo, menu } from "../../../assets/assets";
+import { urlFor } from "../../../client";
+import { useStateContext } from "../../../state/OnLandingContext";
 import { navLinks } from "../../../utils/data";
 import Button from "./Button";
 import ButtonOutline from "./ButtonOutline";
 
 const Navbar = () => {
+  const { logoIMG } = useStateContext();
   const [toggle, setToggle] = useState(false);
+
   return (
     <>
       <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 absolute w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
         <div className="container flex flex-wrap justify-between items-center mx-auto">
-          <a href="https://flowbite.com/" className="flex items-center">
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="mr-3 h-6 sm:h-9"
-              alt="Flowbite Logo"
-            />
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+          <a href="https://jambekcom.com" className="flex items-center">
+            {logoIMG ? (
+              <img
+                src={urlFor(logoIMG)}
+                className="mr-3 sm:h-9"
+                width={100}
+                height={200}
+                alt="Flowbite Logo"
+              />
+            ) : (
+              <img
+                src="https://flowbite.com/docs/images/logo.svg"
+                className="mr-3 h-6 w-6 sm:h-9"
+                alt="Flowbite Logo"
+              />
+            )}
+
+            <span className="self-center text-lg font-poppins font-semibold whitespace-nowrap dark:text-white">
+              MakeMaker
             </span>
           </a>
           <div className="flex md:order-2">
@@ -52,40 +67,19 @@ const Navbar = () => {
             className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1"
             id="navbar-sticky"
           >
-            <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-                  aria-current="page"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  About
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Services
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Contact
-                </a>
-              </li>
+            <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+              {navLinks.map((navLink, index) => (
+                <li key={index + navLink.id} className={"font-poppins"}>
+                  <a
+                    href={`#${navLink.id}`}
+                    className={`font-normal cursor-pointer font-poppins text-[16px]   ${
+                      index === navLinks.length - 1 ? "mr-0" : "mr-10"
+                    } text-indigo-500`}
+                  >
+                    {navLink.title}
+                  </a>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
