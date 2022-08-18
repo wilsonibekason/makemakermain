062dfca9f7881a9d193495e645bf629d90339499
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-18 15:07:52 +0100

    complete add quantity values

--- src/state/OnShopContext.jsx
+++ src/state/OnShopContext.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, createContext, useContext } from "react";
 import { BsStarFill } from "react-icons/bs";
 import { createPopper } from "@popperjs/core";
+import { toast } from "react-hot-toast";
 import {
   addToBasket,
   removeFromBasket,
@@ -38,6 +39,15 @@ export const ShopProvider = ({ children }) => {
     email: "",
     review: "",
   });
+  // cart logic stats
+  // LOGIC STATES
+  // const [showCart, setShowCart] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalQuantities, setTotalQuantities] = useState(0);
+  const [productQuantity, setProductQuantity] = useState(1);
+  // LOGIC STATES
+
   //// dropdown state
   const [isDropDownOpen, setIsDropDownOpen] = useState(false);
   const btnRef = React.useRef();
@@ -128,7 +138,6 @@ export const ShopProvider = ({ children }) => {
 
 console.log(priceSplitter(72500));
      */
-
   //const items = useSelector(selectItems);
   // SECTION FOR FILTERING PRODUCTS
 
@@ -148,6 +157,127 @@ console.log(priceSplitter(72500));
       }
     }, 500);
   };
+
+  ////////// ***************************************************************************************    PRODUCTS MAIN LOGIC ***************************************** ////////////////
+
+  let itemIndex;
+  let foundProduct;
+  //-----------------------ADDING LOGIC---------------------//
+  const onAdd = (product, quantity) => {
+    const checkProductInCart = cartItems.find(
+      (item) => item?._id === product?._id
+    );
+    setTotalPrice((previousPrice) => previousPrice + product?.price * quantity);
+    setTotalQuantities((previousQuantity) => previousQuantity + quantity);
+    if (checkProductInCart) {
+      const updatedCartItems = cartItems.map((cartProduct) => {
+        if (cartProduct?._id == product?._id) {
+          return { ...cartItems, quantity: cartProduct?.quantity + quantity };
+        }
+      });
+      setCartItems(updatedCartItems);
+    } else {
+      product.quantity = quantity;
+      setCartItems([...cartItems, { ...product }]);
+    }
+    toast.success(`${quantity} ${product?.title} added to the cart`);
+  };
+  //-----------------------ADDING LOGIC---------------------//
+
+  //-----------------------REMVING LOGIC---------------------//
+  const onRemove = (product) => {
+    foundProduct = cartItems?.find((item) => item?._id === product?._id);
+    const newCartItem = cartItems?.filter((item) => item?._id !== product?._id);
+    setTotalPrice(
+      (previousPrice) =>
+        previousPrice - foundProduct?.price * foundProduct?.quantity
+    );
+    setTotalQuantities(
+      (previousQuantity) => previousQuantity - foundProduct?.quantity
+    );
+    setCartItems(newCartItem);
+    toast.success(
+      `${foundProduct?.quantity} ${product?.title} removed from the cart`
+    );
+  };
+  //-----------------------REMOVING LOGIC---------------------//
+  //-----------------------CART_ITEMS TOGGLE LOGIC---------------------//
+
+  const toggleCartItemsQuantities = (id, value) => {
+    foundProduct = cartItems?.find((item) => item._id === id);
+    itemIndex = cartItems.findIndex((product) => product?._id === id);
+    const newCartItem = cartItems.filter((item) => item?._id != id);
+    if (value === "increase") {
+      let newCartItems = [
+        ...newCartItem,
+        {
+          ...foundProduct,
+          quantity: foundProduct?.quantity + 1,
+        },
+      ];
+      setCartItems(newCartItems);
+      setTotalPrice((previousPrice) => previousPrice + foundProduct?.price);
+      setTotalQuantities((previousQuantity) => previousQuantity + 1);
+    } else if (value === "decrease") {
+      if (foundProduct?.quantity > 1) {
+        let newCartItems = [
+          ...newCartItem,
+          {
+            ...foundProduct,
+            quantity: foundProduct?.quantity - 1,
+          },
+        ];
+        setCartItems(newCartItems);
+        setTotalPrice((previousPrice) => previousPrice - foundProduct?.price);
+        setTotalQuantities((previousQuantity) => previousQuantity - 1);
+      }
+    }
+  };
+  //-----------------------CART_ITEMS TOGGLE LOGIC---------------------//
+  //-----------------------ADDING LOGIC---------------------//
+  const increaseQuantity = () => {
+    let newCartItem = {
+      ...cartItems[0],
+      quantity: cartItems.length - 1,
+    };
+    setCartItems(newCartItem);
+    setTotalPrice((previousPrice) => previousPrice + (cartItems.length - 1));
+  };
+
+  const increQty = () => {
+    let newCartItem = {
+      ...cartItems[cartItems.length - 1],
+      quantity: cartItems.length - 1,
+    };
+    setCartItems(newCartItem);
+    setTotalQuantities((previousQuantity) => previousQuantity + 1);
+    setTotalPrice(0);
+  };
+  // ----------------------- MAIN INCREASE LOGIC---------------------//
+  const inQTY = () => {
+    setProductQuantity((previousQuantity) => previousQuantity + 1);
+  };
+  //-----------------------MAIN INCREASE LOGIC---------------------//
+  const decreaseQuantity = () => {
+    let newCartItem = {
+      ...cartItems[0],
+      quantity: cartItems.length - 1,
+    };
+    setCartItems(newCartItem);
+    setTotalPrice((previousPrice) => previousPrice - (cartItems.length - 1));
+  };
+  //----------------------- MAIN DECREASE LOGIC---------------------//
+  const decreQuantity = () => {
+    setProductQuantity((previousQuantity) =>
+      previousQuantity - 1 < 1 ? 1 : previousQuantity - 1
+    );
+  };
+  //-----------------------MAIN DECREASE LOGIC---------------------//
+  //-----------------------increase && decrease products  LOGIC---------------------//
+
+  ////////// ***************************************************************************************    PRODUCTS MAIN LOGIC ***************************************** ////////////////
+
+  ////////// ***************************************************************************************    REVIEWS MAIN LOGIC ***************************************** ///////////////
   // SECTION FOR FILTERING PRODUCTS
 
   // FILTER BUTTON ACTIONS
@@ -173,6 +303,16 @@ console.log(priceSplitter(72500));
         isDropDownOpen,
         btnRef,
         popOverRef,
+        // ecommerce logic
+        onAdd,
+        decreQuantity,
+        decreaseQuantity,
+        increQty,
+        increaseQuantity,
+        inQTY,
+        totalPrice,
+        totalQuantities,
+        productQuantity,
       }}
     >
       {children}
