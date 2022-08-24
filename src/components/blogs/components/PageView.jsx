64f9632c6eea5c7811063cbb3ca250a9196d0fdd
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-24 15:29:49 +0100

    complete add stripe setup

---
+++ src/components/blogs/components/PageView.jsx
@@ -0,0 +1,17 @@
+const PageView = () => {
+  return (
+    <>
+      <div
+        className="absolute rounded bg-no-repeat bg-cover black__bg shadow-sm inline-block w-full h-auto"
+        style={{
+          backgroundImage: `url("https://leadership.ng/wp-content/uploads/2022/06/STEM-education.jpeg")`,
+        }}
+      />
+      <div className="absolute items-center ">
+        <h3>Helo world </h3>
+      </div>
+    </>
+  );
+};
+
+export default PageView;
