614899a20a841ce8f677391461fdd8c8f2a80f51
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-25 21:19:08 +0100

    complete edit product home swiper and side widgets

--- src/components/shop/component/Categories.jsx
+++ src/components/shop/component/Categories.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import styles, { blogLayout } from "../../../style";
 
-const Categories = () => {
+const Categories = ({ category }) => {
+  console.log("your category lists", category);
   return (
     <>
       <div className={`${blogLayout.section}`}>
         <h4 className={`${styles.span1}`}> Tags & Categories</h4>
         <div className="border-t bg-black mt-4 my-2" />
+
         <div className="flex flex-row justify-between">
           <div>
             <p className={`${styles.paragraph2}`}>Technology</p>
           </div>
-          <div className="bg-blue-500 rounded-full items-center">
-            <p
-              className={` text-white cursor-pointer font-poppins text-[16px]  
-          py-1 px-1`}
-            >
-              9
-            </p>
+          <div
+            className="bg-blue-500 rounded-full  text-white cursor-pointer font-poppins text-[14px]  
+         flex items-center  justify-center  px-2"
+          >
+            9
           </div>
         </div>
       </div>
