4614c5c4fa89f19a3340c629cb425bb18864e177
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-29 16:34:09 +0100

    complete add value card UI

--- src/components/about/component/AboutContact.jsx
+++ src/components/about/component/AboutContact.jsx
@@ -1,38 +1,48 @@
 import React from "react";
 import styles, { layout } from "../../../style";
+import ValueCard from "./ValueCard";
 const AboutContact = () => {
   return (
     <>
-      {/* <div className="mx-h-[100vh] max-w-auto"> */}
-      <div className={`flex flex-col md:flex-row `}>
-        <div className={`${layout.sectionInfo}`}>
-          <p
-            className={`font-poppins font-semibold text-sm blue__txt uppercase `}
-          >
-            about makemaker_
-          </p>
-          <h1 className={`${styles.heading5}`}>
-            A full service digital STEM agency
-          </h1>
-          <p className={`${styles.paragraph1}`}>
-            Our team of specialists consistently delivers outstanding results
-            combining creative ideas with our vast experience. We can help you
-            build a sustainable, meaningful relationship with your clients by
-            engaging them with your brand using social media. We work in areas
-            as diverse as website design, search engine optimization, social
-            media marketing, email marketing and digital marketing.
-          </p>
+      <div className="min-h-[120vh] max-w-auto">
+        <div className={`flex flex-col md:flex-row `}>
+          <div className={`${layout.sectionInfo}`}>
+            <p
+              className={`font-poppins font-semibold text-sm blue__txt uppercase `}
+            >
+              about makemaker_
+            </p>
+            <h1 className={`${styles.heading5}`}>
+              A full service digital STEM agency
+            </h1>
+            <p className={`${styles.paragraph1}`}>
+              Our team of specialists consistently delivers outstanding results
+              combining creative ideas with our vast experience. We can help you
+              build a sustainable, meaningful relationship with your clients by
+              engaging them with your brand using social media. We work in areas
+              as diverse as website design, search engine optimization, social
+              media marketing, email marketing and digital marketing.
+            </p>
+          </div>
+          <div className={`${layout.sectionImg}`}>
+            <img
+              className="w-full h-full relative z-[20] rounded-lg"
+              // src="https://jambekcom.com/wp-content/uploads/2022/05/Customer-with-smiling-JP200-in-office_Jambekcom-1.jpg "
+              src="https://www.legalcheek.com/wp-content/uploads/2018/11/stem-student.jpeg"
+              alt=""
+            />
+          </div>
         </div>
-        <div className={`${layout.sectionImg}`}>
-          <img
-            className="w-full h-full relative z-[20] rounded-lg"
-            // src="https://jambekcom.com/wp-content/uploads/2022/05/Customer-with-smiling-JP200-in-office_Jambekcom-1.jpg "
-            src="https://www.legalcheek.com/wp-content/uploads/2018/11/stem-student.jpeg"
-            alt=""
-          />
+        {/** core values  */}
+        {/* <section class="bg-gray-900 h-screen flex justify-center items-center gap-x-16 text-white"> */}
+        <div className="flex flex-wrap gap-4 mx-4 lg:mx-16 flex-col lg:flex-row basis-full mt-28">
+          {/* <div className="flex-auto w-full lg:h-[25%]"> */}
+          <ValueCard />
+          <ValueCard />
+          <ValueCard />
         </div>
       </div>
-
+      {/* </section> */}
       {/* </div> */}
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+const Myclass = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".my-rotate-y-180": {
+      transform: "rotateY(180deg)",
+    },
+    ".preserve-3d": {
+      transformStyle: "preserve-3d",
+    },
+    ".perspective": {
+      perspective: "1000px",
+    },
+    ".backface-hidden": {
+      backfaceVisibility: "hidden",
+    },
+  });
+});
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   mode: "jit",
@@ -25,8 +42,10 @@ module.exports = {
       xl: "1700px",
     },
   },
+
   plugins: [
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/line-clamp"),
+    Myclass,
   ],
 };
